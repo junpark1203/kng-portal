1289a5681b76b0d4872711cc32828df96102855e
junpark1203 <junpark1203@gmail.com>
2026-04-23 22:27:20 +0900

    fix: 일괄등록 연동 시 옵션(컬러/사이즈) 매핑 제거 및 정산 항목(판매배송비) 누락 버그 수정

--- Seller K/mass_upload/app-features.js
+++ Seller K/mass_upload/app-features.js
@@ -1715,49 +1715,19 @@ function exportToInventory() {
     
     allProducts.forEach(function(p) {
         if (idsToExport.indexOf(p.id) !== -1) {
-            // Parse options intelligently into color and size
-            var colorStr = '';
-            var sizeStr = '';
-            
-            if (p.optionNames && Array.isArray(p.optionNames) && p.optionValues && Array.isArray(p.optionValues)) {
-                for (var i = 0; i < p.optionNames.length; i++) {
-                    var optName = (p.optionNames[i] || '').toLowerCase();
-                    var vals = p.optionValues[i] || [];
-                    var joined = Array.isArray(vals) ? vals.join(', ') : String(vals);
-                    
-                    if (optName.indexOf('컬러') !== -1 || optName.indexOf('색상') !== -1 || optName.indexOf('color') !== -1) {
-                        colorStr = joined;
-                    } else if (optName.indexOf('사이즈') !== -1 || optName.indexOf('크기') !== -1 || optName.indexOf('size') !== -1) {
-                        sizeStr = joined;
-                    } else {
-                        if (i === 0 && !colorStr) colorStr = joined;
-                        else if (i === 1 && !sizeStr) sizeStr = joined;
-                    }
-                }
-            } else if (p.optionValues && p.optionValues.length > 0) {
-                if (Array.isArray(p.optionValues[0])) {
-                    colorStr = p.optionValues[0].join(', ');
-                    if (p.optionValues.length > 1 && Array.isArray(p.optionValues[1])) {
-                        sizeStr = p.optionValues[1].join(', ');
-                    }
-                } else if (typeof p.optionValues[0] === 'string') {
-                    colorStr = p.optionValues.join(', ');
-                }
-            }
-            
             exportPayload.push({
                 supplier: supplier,
                 brand: p.brand || '',
                 name: p.internalName || p.productName || '',
-                color: colorStr || '',
-                size: sizeStr || '',
+                color: '', // 옵션 매핑 제거 (빈칸 처리)
+                size: '',  // 옵션 매핑 제거 (빈칸 처리)
                 uploadDate: todayStr,
                 buyPrice: p.buyPrice || 0,
                 buyShipping: p.buyShippingFee || 0,
                 shippingBasis: p.deliveryFeeType || '수량별',
                 shippingQty: p.shippingQty || 1,
                 sellPrice: p.salePrice || 0,
-                sellShipping: p.deliveryFee || 0,
+                sellShipping: p.saleShippingFee || 0, // deliveryFee -> saleShippingFee 로 수정
                 isLowestPrice: 0,
                 isSoldOut: 0,
                 remarks: ''
