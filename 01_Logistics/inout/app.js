fe3fdb90115bf075cad2ff13b82ad39ae835b301
junpark1203 <junpark1203@gmail.com>
2026-08-21 09:42:36 +0900

    style: enlarge edit outbound modal and fix readonly inputs layout

--- 01_Logistics/inout/app.js
+++ 01_Logistics/inout/app.js
@@ -1499,9 +1499,9 @@ const app = {
             $('edit_out_shipping').value = item.shipping_fee || 0;
             $('edit_out_note').value = item.note || '';
             
-            $('edit_out_item').value = item.item;
-            $('edit_out_spec').value = item.spec;
-            $('edit_out_unit').value = item.unit;
+            $('edit_out_item').innerText = item.item;
+            $('edit_out_spec').innerText = item.spec || '-';
+            $('edit_out_unit').innerText = item.unit || '-';
             $('edit_out_qty').value = item.qty;
             $('edit_out_price').value = item.selling_price || item.price;
             
@@ -1537,8 +1537,8 @@ const app = {
     
     openEditOutboundLotModal: function() {
         this.currentLotModalRowId = 'editOutbound';
-        const itemName = $('edit_out_item').value;
-        const specName = $('edit_out_spec').value;
+        const itemName = $('edit_out_item').innerText;
+        const specName = $('edit_out_spec').innerText;
         const targetQty = parseFloat($('edit_out_qty').value) || 0;
         
         $('lotModalItemTitle').innerText = `[${itemName} / ${specName}]`;
@@ -1589,9 +1589,9 @@ const app = {
         const payload = {
             date: $('edit_out_date').value,
             destination: $('edit_out_destination').value,
-            item: $('edit_out_item').value,
-            spec: $('edit_out_spec').value,
-            unit: $('edit_out_unit').value,
+            item: $('edit_out_item').innerText,
+            spec: $('edit_out_spec').innerText,
+            unit: $('edit_out_unit').innerText,
             qty: targetQty,
             selling_price: parseFloat($('edit_out_price').value) || 0,
             shipping_fee: parseFloat($('edit_out_shipping').value) || 0,
