16ce192906ea604f6e8d95b0defb90800552d0d9
junpark1203 <junpark1203@gmail.com>
2026-08-24 11:12:17 +0900

    Fix modal layout and button styling in iframe (col-md to col)

--- 01_Logistics/inout/app.js
+++ 01_Logistics/inout/app.js
@@ -452,23 +452,23 @@ const app = {
         const rowId = 'in_row_' + Date.now();
         const rowHtml = `
             <div class="row g-2 mb-1 align-items-center inbound-item-row" id="${rowId}">
-                <div class="col-md-3 position-relative">
+                <div class="col-3 position-relative">
                     <input type="text" class="form-control form-control-sm in-item" placeholder="품목명" autocomplete="off" required>
                     <div class="autocomplete-suggestions" style="display:none;"></div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-3">
                     <input type="text" class="form-control form-control-sm in-spec" placeholder="규격" required>
                 </div>
-                <div class="col-md-3">
+                <div class="col-3">
                     <div class="input-group input-group-sm">
                         <input type="number" class="form-control in-qty" placeholder="수량" min="0.01" step="0.01" required>
                         <input type="text" class="form-control in-unit bg-light text-center" style="max-width: 45px; padding: 0.25rem;" placeholder="단위" required>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-2">
                     <input type="number" class="form-control form-control-sm in-price" placeholder="입고단가" min="0" step="1" required>
                 </div>
-                <div class="col-md-1 d-flex justify-content-end">
+                <div class="col-1 d-flex justify-content-end">
                     <button type="button" class="btn btn-sm btn-outline-danger" onclick="app.removeInboundItemRow('${rowId}')"><i class='bx bx-trash'></i></button>
                 </div>
             </div>
@@ -620,23 +620,23 @@ const app = {
 
         const rowHtml = `
             <div class="row g-2 mb-1 align-items-center direct-item-row" id="${rowId}">
-                <div class="col-md-3 position-relative">
+                <div class="col-3 position-relative">
                     <input type="text" class="form-control form-control-sm dir-item" placeholder="품목명" autocomplete="off" required>
                     <div class="autocomplete-suggestions"></div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-2">
                     <input type="text" class="form-control form-control-sm dir-spec" placeholder="규격" required>
                 </div>
-                <div class="col-md-3">
+                <div class="col-3">
                     <div class="input-group input-group-sm">
                         <input type="number" class="form-control dir-qty" placeholder="수량" min="0.01" step="0.01" required>
                         <input type="text" class="form-control dir-unit bg-light text-center" style="max-width: 45px; padding: 0.25rem;" placeholder="단위" required>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-2">
                     <input type="number" class="form-control form-control-sm dir-in-price" placeholder="매입단가" min="0" step="1" required>
                 </div>
-                <div class="col-md-2 d-flex gap-1 justify-content-end">
+                <div class="col-2 d-flex gap-1 justify-content-end">
                     <input type="number" class="form-control form-control-sm dir-out-price flex-grow-1" placeholder="매출단가" min="0" step="1" required>
                     <button type="button" class="btn btn-sm btn-outline-danger px-2" onclick="app.removeDirectItemRow('${rowId}')"><i class='bx bx-trash'></i></button>
                 </div>
@@ -761,25 +761,25 @@ const app = {
 
         const rowHtml = `
             <div class="row g-2 mb-1 align-items-center outbound-item-row" id="${rowId}">
-                <div class="col-md-3 position-relative">
+                <div class="col-3 position-relative">
                     <input type="text" class="form-control form-control-sm out-item" placeholder="품목명" autocomplete="off" required>
                     <div class="autocomplete-suggestions"></div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-2">
                     <select class="form-select form-select-sm out-spec" disabled required onchange="app.handleOutboundSpecChange('${rowId}', this)">
                         <option value="">품목 먼저 선택</option>
                     </select>
                 </div>
-                <div class="col-md-2">
+                <div class="col-2">
                     <div class="input-group input-group-sm">
                         <input type="number" class="form-control out-qty" placeholder="출고 수량" min="0.01" step="0.01" disabled required onchange="app.handleOutboundQtyChange('${rowId}')" onkeyup="app.handleOutboundQtyChange('${rowId}')">
                         <input type="text" class="form-control out-unit bg-light text-center" style="max-width: 45px; padding: 0.25rem;" placeholder="단위" readonly>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-2">
                     <input type="number" class="form-control form-control-sm out-price" placeholder="단가" min="0" step="1" required>
                 </div>
-                <div class="col-md-3 d-flex gap-1 justify-content-center">
+                <div class="col-3 d-flex gap-1 justify-content-center">
                     <button type="button" class="btn btn-sm btn-outline-primary btn-lot flex-grow-1" onclick="app.openLotModal('${rowId}')" disabled>Lot 설정</button>
                     <button type="button" class="btn btn-sm btn-outline-danger" onclick="app.removeOutboundItemRow('${rowId}')"><i class='bx bx-trash'></i></button>
                 </div>
